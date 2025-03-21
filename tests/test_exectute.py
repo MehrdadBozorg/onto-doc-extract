037c907ee5e6b18c504dbf95450447426c48de5b
Mehrdad Bozorg <bozorgmehrdad@gmail.com>
2025-03-21 21:14:50 +0100

    Add tests

---
+++ tests/test_exectute.py
@@ -0,0 +1,17 @@
+
+from pathlib import Path
+import os 
+from einvoice.execute import read_pdf
+def test():
+    # Get the directory of the current script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Get the parent directory
+    parent_dir = Path(os.path.dirname(current_dir)).parent.parent
+
+    # Define the target directory inside the parent directory
+    target_dir = os.path.join(parent_dir, "samples")
+
+    # Define the file path
+    file_path = Path(target_dir) / "sample.pdf"
+    print(read_pdf(file_path))
